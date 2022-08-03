df594de903968f7eb5dbce8d7265984ff583ffe9
LionelMugisha <lionelmugisha2@gmail.com>
2022-08-03 15:35:59 +0200

    working on dashboards and landing page

--- database/migrations/2022_08_03_095342_create_gallery_table.php
+++ database/migrations/2022_08_03_095342_create_gallery_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateGalleryTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,9 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('gallery', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email');
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('picture');
             $table->timestamps();
         });
     }
@@ -30,6 +27,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('gallery');
     }
 }
---
+++ resources/views/landing/index.blade.php
@@ -0,0 +1,67 @@
+@extends('layouts.app')
+@section('title', 'AAMS')
+@section('content')
+    <div class="h-96 w-full bg-gradient-to-r from-indigo-500 to-indigo-500">      
+        <nav class="border-gray-200 px-2">
+            <div class="container flex justify-evenly justify-between items-center mx-auto">
+                <a href="/" class="flex items-center">
+                    <img src="images/logo2.png" alt="" class="w-64 h-42" />
+                </a>
+                <div class="flex justify-evenly items-center">
+                    <div> 
+                        <a href="#" style="font-family: 'Roboto';"
+                        class="font-bold text-xs md:text-xl md:mr-10 mr-4 text-gray-100">Home
+                        </a>
+                    </div>
+                    <div> 
+                        <a href="#gallery" style="font-family: 'Roboto';"
+                        class="font-bold text-xs md:text-xl md:mr-10 mr-4 text-gray-100">Gallery
+                        </a>
+                    </div>
+                    <div> 
+                        <a href="#aboutus" style="font-family: 'Roboto';"
+                        class="font-bold text-xs md:text-xl md:mr-10 mr-4 text-gray-100">About Us
+                        </a>
+                    </div>
+                    <div> 
+                        <a href="#" style="font-family: 'Roboto';"
+                        class="font-bold text-xs md:text-xl md:mr-10 mr-4 text-gray-100">Login
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </nav>
+        <div class="px-80 ml-10 py-36 mb-5">
+            <p class="text-3xl font-bold tracking-tighter mb-2 text-white">
+                Welcome to Auca Alumni Management System
+            </p>
+            <div class="border border-2 w-48 ml-48 border-blue-300 mt-4"></div>
+        </div>
+    </div>
+    <div class="h-96 w-full" style="background-color:darkgrey;" id="upcomingevents">
+        <div class="flex flex-col justify-center items-center">
+            <p class="text-3xl font-bold tracking-widest mb-2 text-white mt-5">Upcoming Events</p>
+            <div class="border border-2 w-48 ml-18 border-blue-300 mt-1"></div>
+        </div>
+    </div>
+    <div class="h-96 w-full" style="background-color:darkgrey;" id="gallery">
+        <div class="flex flex-col justify-center items-center">
+            <p class="text-3xl font-bold tracking-widest mb-2 text-white mt-5">Gallery</p>
+            <div class="border border-2 w-48 ml-18 border-blue-300 mt-1"></div>
+        </div>
+    </div>
+    <div class="h-96 w-full" style="background-color:darkgrey;" id="aboutus">
+        <div class="flex flex-col justify-center items-center">
+            <p class="text-3xl font-bold tracking-widest mb-2 text-white mt-5">About Us</p>
+            <div class="border border-2 w-48 ml-18 border-blue-300 mt-1"></div>
+        </div>
+    </div>
+    <div class="w-full" style="background-color:darkgrey;" id="footer">
+        <div class="container mx-auto px-6 py-6">
+            <div class="font-bold tracking-wider text-xs md:text-base text-white  text-center">
+                Copyright © 2022 <span>Auca Alumni Management System.</span> All rights reserved.
+            </div>
+        </div>
+    </div>
+@endsection
+

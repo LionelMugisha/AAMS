e3f90b24d261b7e6999849b9293099eaf2ef36bd
LionelMugisha <lionelmugisha2@gmail.com>
2022-09-03 20:09:21 +0200

    minor changes on the dashboards

--- resources/views/components/membersidebar.blade.php
+++ resources/views/components/membersidebar.blade.php
@@ -15,7 +15,7 @@
           <span class="ml-4">Job Opportunities</span>
         </a>
       </li>
-      <li class="relative px-6 py-3">
+      <!-- <li class="relative px-6 py-3">
         <a class="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200" href="{{ route('member.payment') }}">
           <svg class="w-5 h-5" aria-hidden="true" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
             <path d="M11 3.055A9.001 9.001 0 1020.945 13H11V3.055z"></path>
@@ -23,5 +23,5 @@
           </svg>
           <span class="ml-4">Payment</span>
         </a>
-      </li>
+      </li> -->
     </ul>
